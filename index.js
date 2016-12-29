3e1646e2c7ddc1f9f602526513bde6b3aef0a2ee
burawi <burawi.ba@gmail.com>
2016-12-29 21:03:22 +0100

    zindex modules

--- index.js
+++ index.js
@@ -4,16 +4,26 @@ var fs = require('fs');
 var path = require('path');
 var capitalize = require('capitalize');
 
-var forEachModule = function(fn) {
+var loadModules = function () {
+    // Sorting was inspired by this SO answer: http://stackoverflow.com/a/1069840/2952266
+    var res = [];
     var modulesDir = fs.readdirSync('./app_modules');
     modulesDir.forEach(function(element) {
-        var stat = fs.lstatSync(path.resolve('.', 'app_modules', element));
+        var modulePath = path.resolve('.', 'app_modules', element);
+        var stat = fs.lstatSync(modulePath);
         if (stat.isDirectory()) {
-            fn(element);
-        } else if (stat.isFile()) {
-            // console.log(element+': belongs to main module');
+            var confPath = filePathOf(element,'conf.js');
+            var zindex = (fs.existsSync(confPath)) ? require(confPath)().zindex : 0;
+            res.push([ element, zindex || 0]);
         }
     });
+    res.sort(function(a, b) {
+        return b[1] - a[1]
+    });
+    for (var i = 0; i < res.length; i++) {
+        res[i] = res[i][0];
+    }
+    return res;
 }
 
 var filePathOf = function(moduleName, fileName) {
@@ -28,7 +38,7 @@ var fileOf = function(moduleName, fileName, ...args) {
         }else {
             return require(pathToFile)(...args);
         }
-    }else {
+    } else {
         return function(){};
     }
 }
@@ -47,12 +57,12 @@ var forEachFileOf = function (moduleName, dirName, fn) {
     }
 }
 
-var loadViewsOf = function (moduleName, type) {
+var loadViewsOf = function (moduleName, type, modules) {
     var views = {};
     forEachFileOf(moduleName, 'views/'+type, function (fileName,pathToFile) {
         var plObjectName = capitalize(type).slice(0, -1);
         var view = new pl[plObjectName](pathToFile)
-        forEachModule(function (moduleName) {
+        modules.forEach(function (moduleName) {
             forEachFileOf(moduleName,'views/components',function (fileName,pathToFile) {
                 view.includeAtTop(pathToFile);
             });
@@ -62,64 +72,6 @@ var loadViewsOf = function (moduleName, type) {
     return views;
 }
 
-var loadCommonFiles = function(fileName, extra) {
-    var files = {};
-    forEachModule(function(moduleName) {
-        try {
-            files[moduleName] = fileOf(moduleName, fileName, extra);
-        } catch (e) {}
-    });
-    return files;
-}
-
-var getModulePages = function (moduleName) {
-    var res = {};
-    try {
-        var pagesDir = fs.readdirSync(path.resolve('.', 'app_modules', moduleName, "pages"));
-        pagesDir.forEach(function(element) {
-            var stat = fs.lstatSync(path.resolve('.', 'app_modules', moduleName, element));
-            if (stat.isDirectory()) {
-            } else if (stat.isFile()) {
-                try {
-                    res.push(new pl.Page(path.resolve('.', 'app_modules', moduleName, element)))
-                } catch (e) { }
-            }
-        });
-    } catch (e) { }
-}
-
-var loadModule = function(moduleName, bag) {
-    var module = {
-        prefix: '/' + moduleName,
-        router: fileOf(moduleName, 'routes', bag),
-        client: './app_modules/' + moduleName + '/client',
-    };
-    return module;
-}
-
-var loadAppModules = function(app, extra) {
-    var bag = extra;
-    bag.nidam = require('./helpers.js')(extra);
-    bag.app = app;
-    forEachModule(function(moduleName) {
-        bag[moduleName] = {
-            conf: fileOf(moduleName, 'conf', extra),
-            pages: getModulePages(moduleName)
-        }
-        var moduleArgs = extra;
-        moduleArgs[moduleName] = bag[moduleName];
-        bag[moduleName].middlewares = fileOf(moduleName, 'middlewares', moduleArgs);
-        bag[moduleName].helpers = fileOf(moduleName, 'helpers', moduleArgs);
-    });
-    bag.root = bag[''];
-    delete bag[''];
-    var modules = [];
-    forEachModule(function(moduleName) {
-        modules.push(loadModule(moduleName, bag));
-    });
-    return modules;
-}
-
 module.exports = {
     use: function(app, args) {
         var bag = {};
@@ -127,12 +79,13 @@ module.exports = {
         bag.E = args;
         bag.nidam = require('./F.js')(bag);
         var checkpointArgs = bag;
-        forEachModule(function (moduleName) {
+        var modules = loadModules();
+        modules.forEach(function (moduleName) {
             bag[moduleName] = {
                 conf: fileOf(moduleName,'conf.js', checkpointArgs),
                 V: {
-                    L: loadViewsOf(moduleName, 'layouts'),
-                    P: loadViewsOf(moduleName, 'pages')
+                    L: loadViewsOf(moduleName, 'layouts', modules),
+                    P: loadViewsOf(moduleName, 'pages', modules)
                 }
             };
             if(bag[moduleName].conf.prefix === undefined){
@@ -140,10 +93,10 @@ module.exports = {
             }
         });
         checkpointArgs = bag;
-        forEachModule(function (moduleName) {
+        modules.forEach(function (moduleName) {
             bag[moduleName].F = fileOf(moduleName,'F.js', checkpointArgs, bag[moduleName].conf);
         });
-        forEachModule(function (moduleName) {
+        modules.forEach(function (moduleName) {
             var pathToModule = path.resolve('.', 'app_modules', moduleName)
             if (fs.existsSync(pathToModule + '/index.js')) {
                 require(pathToModule)(bag, bag[moduleName]);
